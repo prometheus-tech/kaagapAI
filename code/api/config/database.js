0bc8220c2ad0da605b475795265f4e8290753110
Jody Adriene Dong-e <2160809@slu.edu.ph>
2019-02-19 20:59:12 +0800

    Modified database configuration file and connected it to database

--- code/api/config/database.js
+++ code/api/config/database.js
@@ -1,10 +1,24 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '',
-  user: '',
-  database: '',
-  password: ''
+  host: 'localhost',
+  port: '3306',
+  //host: '',
+  user: 'admin',
+  password: 'adminkaagapai',
+  database: 'db_kaagapai'
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  if (connection) {
+    connection.release();
+    console.log('db_kaagapai connected');
+  }
+  return;
 });
 
 module.exports = pool.promise();
