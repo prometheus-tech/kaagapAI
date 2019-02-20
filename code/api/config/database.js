23bb57e3627b05472bf87349a6f8e88b40b24da7
Jody Adriene Dong-e <2160809@slu.edu.ph>
2019-02-20 08:16:32 +0800

    Added 'operatorsAliases' to line of code in database.js to override deprecation error

--- code/api/config/database.js
+++ code/api/config/database.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('db_kaagapai', 'admin', 'adminkaagapai', {
   dialect: 'mysql',
   host: 'localhost',
-  port: '3306'
+  port: '3306',
+  operatorsAliases: false
 });
 
 module.exports = sequelize;
