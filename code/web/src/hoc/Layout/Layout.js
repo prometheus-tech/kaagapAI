6ecb26f0a3ddfb248a7d74910fee45aab67e9017
julianaubrey <2161495@slu.edu.ph>
2019-02-22 23:42:22 +0800

    Added padding to the mainGrid styles

--- code/web/src/hoc/Layout/Layout.js
+++ code/web/src/hoc/Layout/Layout.js
@@ -3,14 +3,26 @@ import React from 'react';
 import Auxilliary from '../Auxilliary/Auxilliary';
 import Header from '../../components/Navigation/Header/Header';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  mainGrid: {
+    paddingRight: 24,
+    paddingLeft: 24
+  }
+});
 
 function Layout(props) {
+  const { classes } = props;
+
   return (
     <Auxilliary>
       <Header />
-      <Grid container>{props.children}</Grid>
+      <Grid container className={classes.mainGrid}>
+        {props.children}
+      </Grid>
     </Auxilliary>
   );
 }
 
-export default Layout;
+export default withStyles(styles)(Layout);
