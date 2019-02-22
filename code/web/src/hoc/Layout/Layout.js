7dbc42289ed58190bdda7e6ecc41a15267dc867a
julianaubrey <2161495@slu.edu.ph>
2019-02-22 23:36:54 +0800

    Edited Layout.js

--- code/web/src/hoc/Layout/Layout.js
+++ code/web/src/hoc/Layout/Layout.js
@@ -1,50 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 
 import Auxilliary from '../Auxilliary/Auxilliary';
 import Header from '../../components/Navigation/Header/Header';
+import Grid from '@material-ui/core/Grid';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    height: 140,
-    width: 100
-  },
-  control: {
-    padding: theme.spacing.unit * 2
-  }
-});
-
-class GuttersGrid extends React.Component {
-  state = {
-    spacing: '16'
-  };
-
-  handleChange = key => (event, value) => {
-    this.setState({
-      [key]: value
-    });
-  };
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <Auxilliary>
-        <Header />
-        <Grid container className={classes.root}>
-          {this.props.children}
-        </Grid>
-      </Auxilliary>
-    );
-  }
+function Layout() {
+  return (
+    <Auxilliary>
+      <Header />
+      <Grid container>{this.props.children}</Grid>
+    </Auxilliary>
+  );
 }
 
-GuttersGrid.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(GuttersGrid);
+export default Layout;
