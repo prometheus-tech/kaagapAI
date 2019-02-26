c7ca9786148417617430c350010450937c3d92f3
erikatorio <2160120@slu.edu.ph>
2019-02-26 19:46:29 +0800

    Revised seeder file for sessions

--- code/api/seeders/20190226113323-demo-session.js
+++ code/api/seeders/20190226113323-demo-session.js
@@ -4,42 +4,42 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Sessions', [{
       session_name: 'Session 1',
-      date_of_session: '02-26-2019',
+      date_of_session: '2019-02-26',
       c_id: 1
     },
     {
       session_name: 'Session 2',
-      date_of_session: '03-04-2019',
+      date_of_session: '2019-03-04',
       c_id: 1
     },
     {
       session_name: 'Session 3',
-      date_of_session: '03-14-2019',
+      date_of_session: '2019-03-14',
       c_id: 1
     },
     {
       session_name: 'Session 1',
-      date_of_session: '02-27-2019',
+      date_of_session: '2019-02-27',
       c_id: 2
     },
     {
       session_name: 'Session 2',
-      date_of_session: '03-15-2019',
+      date_of_session: '2019-03-15',
       c_id: 2
     },
     {
       session_name: 'Session 1',
-      date_of_session: '02-26-2019',
+      date_of_session: '2019-02-26',
       c_id: 3
     },
     {
       session_name: 'Session 2',
-      date_of_session: '03-06-2019',
+      date_of_session: '2019-03-06',
       c_id: 3
     },
     {
       session_name: 'Session 3',
-      date_of_session: '03-18-2019',
+      date_of_session: '2019-03-18',
       c_id: 3
     },
     {
