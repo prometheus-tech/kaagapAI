8244653b07ec8e86466e8882b7526a303fd11671
julianaubrey <2161495@slu.edu.ph>
2019-03-02 03:49:39 +0800

    Added http:// prefix in Apollo Client uri

--- code/web/src/App.js
+++ code/web/src/App.js
@@ -6,7 +6,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'kaagapai.com:4000/graphql'
+  uri: 'http://kaagapai.com:4000/graphql'
 });
 
 class App extends Component {
