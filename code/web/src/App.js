1901f04afb472ce0792d6b1164c0f72bf902d64e
julianaubrey <2161495@slu.edu.ph>
2019-03-06 02:04:51 +0800

    Added currentUserId mock data

--- code/web/src/App.js
+++ code/web/src/App.js
@@ -7,18 +7,26 @@ import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
 import Layout from './hoc/Layout/Layout';
 import ClientsPage from './containers/ClientsPage/ClientsPage';
 
+const cache = new InMemoryCache({
+  dataIdFromObject: object => {
+    switch (object.__typename) {
+      case 'Client':
+        return object.c_id;
+      default:
+        return defaultDataIdFromObject(object);
+    }
+  }
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache({
-    dataIdFromObject: object => {
-      switch (object.__typename) {
-        case 'Client':
-          return object.c_id;
-        default:
-          return defaultDataIdFromObject(object);
-      }
-    }
-  })
+  cache
+});
+
+cache.writeData({
+  data: {
+    currentUserId: parseInt(1)
+  }
 });
 
 class App extends Component {
