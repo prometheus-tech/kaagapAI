d20649af737982b79433be6c3c823527ef3bfb33
julianaubrey <2161495@slu.edu.ph>
2019-03-28 00:40:29 +0800

    Added configurations for Apollo Server

--- code/api/app.js
+++ code/api/app.js
@@ -4,23 +4,59 @@ import typeDefs from './graphql/schemas/schema';
 import resolvers from './graphql/resolvers/resolvers';
 import models from './models';
 import cors from 'cors';
+import http from 'http';
 
-const server = new ApolloServer({
+const configurations = {
+  development: {
+    ssl: false,
+    port: process.env.PORT || 4000,
+    hostname: process.env.DB_HOST_DEV
+  },
+  test: {
+    ssl: false,
+    port: process.env.PORT || 4000,
+    hostname: process.env.DB_HOST_TEST
+  },
+  production: {
+    ssl: false,
+    port: process.env.PORT || 4000,
+    hostname: process.env.DB_HOST_PROD
+  }
+};
+
+const environment = 'development'; // Change this Jessie
+const config = configurations[environment];
+
+const apollo = new ApolloServer({
   typeDefs: gql(typeDefs),
   resolvers,
   context: { models }
 });
 
 const app = express();
-
 app.use(cors());
-server.applyMiddleware({ app });
+
+apollo.applyMiddleware({ app });
+
+var server;
+if (config.ssl) {
+  console.log(
+    'SSL not yet supported. Please refer to this link: https://www.apollographql.com/docs/apollo-server/essentials/server'
+  );
+} else {
+  server = http.createServer(app);
+}
 
 models.sequelize
   .sync()
-  .then(result => {
-    app.listen(process.env.PORT || 4000, () => {
-      console.log('Server is running at port 4000');
+  .then(res => {
+    server.listen({ port: config.port }, () => {
+      console.log(
+        '🚀  Server ready at',
+        `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
+          apollo.graphqlPath
+        }`
+      );
     });
   })
   .catch(err => {
