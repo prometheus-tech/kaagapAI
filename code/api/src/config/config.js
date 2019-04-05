5ef1f1bb0ae99fab6f4d050a27c42e16a518b098
erikatorio <2160120@slu.edu.ph>
2019-04-05 20:12:23 +0800

    minor revisions to env

--- code/api/src/config/config.js
+++ code/api/src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USERNAME_DEV,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DEV,
-    host: process.env.HOST_DEV,
+    host: process.env.DB_HOST_DEV,
     dialect: 'mysql',
     operatorsAliases: false
   },
@@ -13,7 +13,7 @@ module.exports = {
     username: process.env.DB_USERNAME_TEST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
-    host: process.env.HOST_TEST,
+    host: process.env.DB_HOST_TEST,
     dialect: 'mysql',
     operatorsAliases: false
   },
@@ -21,7 +21,7 @@ module.exports = {
     username: process.env.DB_USERNAME_PROD,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PROD,
-    host: process.env.HOST_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: 'mysql',
     operatorsAliases: false
   }
