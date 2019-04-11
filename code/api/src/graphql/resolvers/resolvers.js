57e07ee10194647d9117ca9e54f16f74e7a5097a
Jody Adriene Dong-e <2160809@slu.edu.ph>
2019-04-11 21:45:50 +0800

    Finished login component authentication

--- code/api/src/graphql/resolvers/resolvers.js
+++ code/api/src/graphql/resolvers/resolvers.js
@@ -2,7 +2,8 @@ import { mergeResolvers } from 'merge-graphql-schemas';
 import clientResolver from './client';
 import sessionResolver from './session';
 import sessionDocumentResolver from './session_document';
+import practitionerResolver from './practitioner';
 
-const resolvers = [clientResolver, sessionResolver, sessionDocumentResolver];
+const resolvers = [clientResolver, sessionResolver, sessionDocumentResolver, practitionerResolver];
 
 export default mergeResolvers(resolvers);
