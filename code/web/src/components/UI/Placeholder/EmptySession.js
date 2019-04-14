a50e2c45d4abfd1684253575b6addea8792c50cc
JessieMartinTabilisma <2164631@slu.edu.ph>
2019-04-14 14:56:56 +0800

    Redesign Breadcrumb and Added Search bar, Added illustration for empty session

--- code/web/src/components/UI/Placeholder/EmptySession.js
+++ code/web/src/components/UI/Placeholder/EmptySession.js
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import EmptySessionImg from '../../../assets/empty_session.svg';
 import Typography from '@material-ui/core/Typography';
-import Fab from '@material-ui/core/Fab';
 import Add from '@material-ui/icons/Add';
-import orange from '@material-ui/core/colors';
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import grey from '@material-ui/core/colors/grey';
 
 const styles = theme => ({
   emptyContainer: {
+    display: 'inline-block',
+    width: '100%',
+    textAlign: 'center'
+  },
+  imgEmpty: {
     display: 'flex',
     justifyContent: 'center'
   },
   emptyImg: {
-    marginTop: theme.spacing.unit * 2,
-    height: '50vh'
+    marginTop: theme.spacing.unit * 5,
+    height: '48vh'
+  },
+  actionSaying: {
+    display: 'inline-block',
+    textAlign: 'center'
   },
   sayingEmpty: {
-    marginTop: theme.spacing.unit * 15,
-    fontSize: theme.spacing.unit * 5,
-    marginRight: theme.spacing.unit * 2
+    marginTop: theme.spacing.unit * 5,
+    fontSize: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 2,
+    letterSpacing: '2px',
+    fontWeight: '300',
+    color: grey[400]
   },
   extendedButton: {
-    background: orange[800],
     color: '#ffffff',
+    background: '#ef6c00',
     textTransform: 'capitalize',
     borderRadius: '50px',
     fontSize: 16,
     '&:hover': {
-      backgroundColor: orange[900],
+      backgroundColor: '#e65100',
       boxShadow: theme.shadows[10]
     },
     marginTop: theme.spacing.unit * 4,
@@ -39,9 +51,16 @@ class EmptySession extends Component {
     const { classes, newSessionOpened } = this.props;
     return (
       <div className={classes.emptyContainer}>
-        <div>
+        <div className={classes.imgEmpty}>
+          <img
+            src={EmptySessionImg}
+            className={classes.emptyImg}
+            alt="Empty client"
+          />
+        </div>
+        <div className={classes.actionSaying}>
           <Typography variant="h4" className={classes.sayingEmpty}>
-            Let's be productive
+            Let's be productive and stay positive....
           </Typography>
           <Fab
             color="primary"
@@ -52,7 +71,6 @@ class EmptySession extends Component {
             <Add className={classes.extendedIcon} /> New Session
           </Fab>
         </div>
-        <img className={classes.emptyImg} src={EmptySessionImg} alt="" />
       </div>
     );
   }
