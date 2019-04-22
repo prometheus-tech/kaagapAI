e143d1db782c1ff7162fcb5f2f8d43c2f95dad4a
JessieMartinTabilisma <2164631@slu.edu.ph>
2019-04-23 02:03:59 +0800

    Added forgot password UI

--- code/web/src/containers/SignIn/SignIn.js
+++ code/web/src/containers/SignIn/SignIn.js
@@ -11,10 +11,11 @@ import Grid from '@material-ui/core/Grid';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 import Typography from '@material-ui/core/Typography';
-import animationLog from '../../assets/animationLog.svg';
+import loginAnimation from '../../assets/login_animation.svg';
 import Logo from '../../assets/kaagapai-logo.svg';
 import blue from '@material-ui/core/colors/blue';
 import grey from '@material-ui/core/colors/grey';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 import Icon from '@material-ui/core/Icon';
 import Fab from '@material-ui/core/Fab';
 
@@ -35,39 +36,33 @@ const styles = theme => ({
     height: 60
   },
   signIn: {
-    marginTop: '10vh',
-    marginLeft: theme.spacing.unit * 5,
     fontSize: theme.spacing.unit * 4,
     color: blue[900]
   },
-  signInInformation: {
-    marginTop: theme.spacing.unit * 2,
-    marginLeft: theme.spacing.unit * 5,
-    fontSize: theme.spacing.unit * 2,
+  signInWelcome: {
+    marginTop: theme.spacing.unit * 5,
+    fontSize: theme.spacing.unit * 1.8,
     fontWeight: '300',
-    color: grey[600],
+    color: blue[400],
     lineHeight: '150%'
   },
   formContainer: {
     backgroundColor: 'rgb(252, 252, 252)'
   },
-  formItems: {
-    marginLeft: theme.spacing.unit * 5
-  },
+  formItems: {},
   illustrationContainer: {
     display: 'inline',
     textAlign: 'center'
   },
   sigInIllustration: {
-    height: '70vh',
-    marginTop: '10vh'
+    height: '90vh',
+    marginTop: '8vh'
   },
   inputActionField: {
     marginTop: theme.spacing.unit * 4,
     fontWeight: '300'
   },
   fab: {
-    margin: theme.spacing.unit,
     borderRadius: '50px',
     padding: '10px 45px 10px 45px',
     backgroundColor: blue[600],
@@ -77,7 +72,8 @@ const styles = theme => ({
     }
   },
   alignMent: {
-    textAlign: 'right'
+    textAlign: 'right',
+    marginTop: theme.spacing.unit * 5
   },
   createAccount: {
     display: 'flex',
@@ -93,6 +89,17 @@ const styles = theme => ({
     '&:hover': {
       color: blue[800]
     }
+  },
+  forgotPassword: {
+    cursor: 'pointer',
+    textDecoration: 'underline',
+    color: blue[900],
+    letterSpacing: '1px',
+    marginTop: theme.spacing.unit * 2,
+    fontWeight: '300'
+  },
+  test: {
+    backgroundColor: 'green'
   }
 });
 class SignIn extends Component {
@@ -141,79 +148,88 @@ class SignIn extends Component {
                     })}
                   </Grid>
                 ) : null}
-                <Grid item xs={4} className={classes.formContainer}>
+                <Grid item xs={5} className={classes.formContainer}>
                   <img src={Logo} className={classes.logo} alt="kaagapAI" />
-                  <Typography className={classes.signIn}>
-                    Login to kaagapAI.
-                  </Typography>
-                  <Typography className={classes.signInInformation}>
-                    To Login fill in the the necessary information, please
-                    provide the necessary message here.
-                  </Typography>
-                  <Grid
-                    container
-                    spacing={8}
-                    className={classes.formItems}
-                    alignContent="center"
-                  >
+                  <Grid container>
+                    <Grid item xs={1} />
                     <Grid item xs={10}>
-                      <TextValidator
-                        label="Email"
-                        margin="dense"
-                        autoComplete="email"
-                        fullWidth
-                        name="email"
-                        value={email}
-                        validators={['required', 'isEmail']}
-                        errorMessages={['Enter an email', 'Invalid email']}
-                        disabled={loading}
-                        onChange={this.inputChangeHandler}
-                        className={classes.inputActionField}
-                      />
-                      <TextValidator
-                        label="Password"
-                        margin="dense"
-                        type="password"
-                        fullWidth
-                        name="password"
-                        value={password}
-                        validators={['required']}
-                        errorMessages={[
-                          'Enter a password',
-                          'Incorrect password'
-                        ]}
-                        disabled={loading}
-                        onChange={this.inputChangeHandler}
-                        className={classes.inputActionField}
-                      />
-                    </Grid>
-                    <Grid item xs={10} className={classes.alignMent}>
-                      <Fab
-                        variant="extended"
-                        aria-label="Delete"
-                        className={classes.fab}
-                        type="submit"
-                      >
-                        {loading ? 'Logging in...' : 'Login'}
-                        <Icon>arrow_right_alt</Icon>
-                      </Fab>
-                    </Grid>
-                    <Grid item className={classes.createAccount} xs={12}>
-                      <Typography className={classes.createQuestion}>
-                        Don't have an account ?
+                      <Typography className={classes.signInWelcome}>
+                        Welcome to.
                       </Typography>
-                      <Typography
-                        className={classes.createAccountButton}
-                        component={SignInButtonLink}
-                      >
-                        Create account
+                      <Typography className={classes.signIn}>
+                        kaagapAI.
                       </Typography>
+                      <Grid
+                        container
+                        spacing={8}
+                        className={classes.formItems}
+                        alignContent="center"
+                      >
+                        <Grid item xs={10}>
+                          <TextValidator
+                            label="Email"
+                            margin="dense"
+                            autoComplete="email"
+                            fullWidth
+                            name="email"
+                            value={email}
+                            validators={['required', 'isEmail']}
+                            errorMessages={['Enter an email', 'Invalid email']}
+                            disabled={loading}
+                            onChange={this.inputChangeHandler}
+                            className={classes.inputActionField}
+                          />
+                          <TextValidator
+                            label="Password"
+                            margin="dense"
+                            type="password"
+                            fullWidth
+                            name="password"
+                            value={password}
+                            validators={['required']}
+                            errorMessages={[
+                              'Enter a password',
+                              'Incorrect password'
+                            ]}
+                            disabled={loading}
+                            onChange={this.inputChangeHandler}
+                            className={classes.inputActionField}
+                          />
+                        </Grid>
+                        <Grid item xs={6}>
+                          <Typography className={classes.forgotPassword}>
+                            Forgot password?
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={12} className={classes.alignMent}>
+                          <Fab
+                            variant="extended"
+                            aria-label="Delete"
+                            className={classes.fab}
+                            type="submit"
+                          >
+                            {loading ? 'Logging in...' : 'Login'}
+                            <Icon>arrow_right_alt</Icon>
+                          </Fab>
+                        </Grid>
+                        <Grid item className={classes.createAccount} xs={12}>
+                          <Typography className={classes.createQuestion}>
+                            Don't have an account ?
+                          </Typography>
+                          <Typography
+                            className={classes.createAccountButton}
+                            component={SignInButtonLink}
+                          >
+                            Create account
+                          </Typography>
+                        </Grid>
+                      </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
-                <Grid item className={classes.illustrationContainer} xs={8}>
+                <Grid item className={classes.illustrationContainer} xs={7}>
                   <img
-                    src={animationLog}
+                    src={loginAnimation}
                     className={classes.sigInIllustration}
                     alt="Sign in illustration"
                   />
