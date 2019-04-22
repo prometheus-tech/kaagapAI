ef4c08bfbf216d47bdb46eb94315e4f9e99a8982
Jody Adriene Dong-e <2160809@slu.edu.ph>
2019-04-22 15:21:21 +0800

    Modified results if nul documents

--- code/api/src/graphql/resolvers/result.js
+++ code/api/src/graphql/resolvers/result.js
@@ -62,81 +62,85 @@ export default {
                 session_id,
                 status: 'active'
               }
-            }).then(results => {
-              results.forEach((result) => {
-                contents.push(result.content);
+            })
+
+            if(!getSessionDocumentContent){
+              return res;
+            } else {
+              await getSessionDocuments.forEach((sessionDocument) => {
+                contents.push(sessionDocument.content);
               });
-            });
-  
-            const addResultRes = await models.Result.create({
-              date_generated: new Date(),
-              session_id
-            });
-  
-            const { result_id } = addResultRes.dataValues;
-  
-            await nluModules.analyzeContent(contents)
-              .then(async response => {
-  
-                //store sentiment to db
-                await models.Sentiment.create({
-                  score: response.sentiment.document.score,
-                  label: response.sentiment.document.label,
-                  result_id
-                });
-  
-                //store keywords to db
-                var keywordsRes = response.keywords;
-  
-                keywordsRes.forEach(async (keywordRes) => {
-                  await models.Keyword.create({
-                    text: keywordRes.text,
-                    relevance: keywordRes.relevance,
-                    count: keywordRes.count,
+    
+              const addResultRes = await models.Result.create({
+                date_generated: new Date(),
+                session_id
+              });
+    
+              const { result_id } = addResultRes.dataValues;
+    
+              await nluModules.analyzeContent(contents)
+                .then(async response => {
+    
+                  //store sentiment to db
+                  await models.Sentiment.create({
+                    score: response.sentiment.document.score,
+                    label: response.sentiment.document.label,
                     result_id
                   });
-                });
-  
-                //store categories to db
-                var categoriesRes = response.categories;
-  
-                categoriesRes.forEach(async (categoryRes) => {
-                  await models.Category.create({
-                    score: categoryRes.score,
-                    label: categoryRes.label,
+    
+                  //store keywords to db
+                  var keywordsRes = response.keywords;
+    
+                  keywordsRes.forEach(async (keywordRes) => {
+                    await models.Keyword.create({
+                      text: keywordRes.text,
+                      relevance: keywordRes.relevance,
+                      count: keywordRes.count,
+                      result_id
+                    });
+                  });
+    
+                  //store categories to db
+                  var categoriesRes = response.categories;
+    
+                  categoriesRes.forEach(async (categoryRes) => {
+                    await models.Category.create({
+                      score: categoryRes.score,
+                      label: categoryRes.label,
+                      result_id
+                    });
+                  });
+    
+                  //store entities to db
+                  var entitiesRes = response.entities;
+    
+                  entitiesRes.forEach(async (entityRes) => {
+                    await models.Entity.create({
+                      type: entityRes.type,
+                      text: entityRes.text,
+                      relevance: entityRes.relevance,
+                      result_id
+                    });
+                  });
+    
+                  //store emotions to db
+                  await models.Emotion.create({
+                    sadness: response.emotion.document.emotion.sadness,
+                    anger: response.emotion.document.emotion.anger,
+                    joy: response.emotion.document.emotion.joy,
+                    fear: response.emotion.document.emotion.fear,
+                    disgust: response.emotion.document.emotion.disgust,
                     result_id
                   });
+    
+                  return result_id;
                 });
-  
-                //store entities to db
-                var entitiesRes = response.entities;
-  
-                entitiesRes.forEach(async (entityRes) => {
-                  await models.Entity.create({
-                    type: entityRes.type,
-                    text: entityRes.text,
-                    relevance: entityRes.relevance,
-                    result_id
-                  });
+    
+                return await models.Result.findOne({
+                  raw: true,
+                  where: { result_id }
                 });
-  
-                //store emotions to db
-                await models.Emotion.create({
-                  sadness: response.emotion.document.emotion.sadness,
-                  anger: response.emotion.document.emotion.anger,
-                  joy: response.emotion.document.emotion.joy,
-                  fear: response.emotion.document.emotion.fear,
-                  disgust: response.emotion.document.emotion.disgust,
-                  result_id
-                });
-  
-                return result_id;
-              });
-  
-              return await models.Result.findOne({
-                raw: true,
-                where: { result_id }
-              });
+            }
           } else {
             const session = await models.Session.findOne({
               where: { session_id: res.session_id }
