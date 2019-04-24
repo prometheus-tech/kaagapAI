c780bfe9225fbbd7c2acffa6bbdc68237b4b8e71
julianaubrey <2161495@slu.edu.ph>
2019-04-24 13:53:57 +0800

    Added button for finding file on sentence map item

--- code/web/src/containers/SessionPage/SessionResultsPage/KeywordsContainer/KeywordMapper/DocumentMapItem/DocumentMapItem.js
+++ code/web/src/containers/SessionPage/SessionResultsPage/KeywordsContainer/KeywordMapper/DocumentMapItem/DocumentMapItem.js
@@ -6,6 +6,8 @@ class DocumentMapItem extends Component {
   render() {
     const { document, keyword } = this.props;
 
+    const { sd_id, file_name, type } = document;
+
     return (
       <div>
         {document.matchingSentences.map((sentence, index) => {
@@ -14,6 +16,9 @@ class DocumentMapItem extends Component {
               key={index}
               keyword={keyword}
               sentence={sentence}
+              sd_id={sd_id}
+              file_name={file_name}
+              type={type}
             />
           );
         })}
--- code/web/src/containers/SessionPage/SessionResultsPage/KeywordsContainer/KeywordMapper/DocumentMapItem/SentenceMapItem/SentenceMapItem.js
+++ code/web/src/containers/SessionPage/SessionResultsPage/KeywordsContainer/KeywordMapper/DocumentMapItem/SentenceMapItem/SentenceMapItem.js
@@ -1,17 +1,47 @@
 import React from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
+
 import Highlighter from 'react-highlight-words';
+import IconButton from '@material-ui/core/IconButton';
+import FindInPageIcon from '@material-ui/icons/FindInPage';
+import purple from '@material-ui/core/colors/purple';
+
+const styles = theme => ({
+  sentenceMapItem: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    marginBottom: 24
+  },
+  findInPageIconButton: {
+    backgroundColor: purple[500],
+    color: 'white'
+  },
+  highlighter: {
+    display: 'block',
+    paddingBottom: theme.spacing.unit,
+    borderBottom: '1px solid #e8e8e8',
+    marginRight: theme.spacing.unit * 2
+  }
+});
 
 function SentenceMapItem(props) {
-  const { document, keyword, sentence } = props;
+  const { keyword, sentence, sd_id, file_name, type, classes } = props;
+
   return (
-    <Highlighter
-      style={{ display: 'block', marginBottom: 24 }}
-      searchWords={[keyword]}
-      autoEscape={true}
-      textToHighlight={sentence + '...'}
-    />
+    <div className={classes.sentenceMapItem}>
+      <Highlighter
+        className={classes.highlighter}
+        searchWords={[keyword]}
+        autoEscape={true}
+        textToHighlight={'"' + sentence + '..."'}
+      />
+      <IconButton className={classes.findInPageIconButton}>
+        <FindInPageIcon />
+      </IconButton>
+    </div>
   );
 }
 
-export default SentenceMapItem;
+export default withStyles(styles)(SentenceMapItem);
