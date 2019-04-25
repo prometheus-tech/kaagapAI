d20bad83b701b0575a5f8c9d809145a27fdb217d
julianaubrey <2161495@slu.edu.ph>
2019-04-25 23:09:45 +0800

    Changed session document card

--- code/web/src/components/Session/SessionDocumentCards/SessionDocumentCard/SessionDocumentCard.js
+++ code/web/src/components/Session/SessionDocumentCards/SessionDocumentCard/SessionDocumentCard.js
@@ -15,10 +15,16 @@ import { getSessionDocumentIcon } from '../../../../util/helperFunctions';
 
 const styles = theme => ({
   card: {
+    marginTop: '1rem',
+    boxShadow: '0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05)',
+    transition:
+      '.3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12)',
     '&:hover': {
+      transform: 'scale(1.05)',
+      boxShadow: '0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06)',
+      padding: '0px 0px 0px 0px',
       cursor: 'pointer'
-    },
-    marginTop: '1rem'
+    }
   },
   cardContent: {
     display: 'flex',
