fe3156467d54cf08e3a5a4ad0a631242496ffd60
julianaubrey <2161495@slu.edu.ph>
2019-04-27 01:43:33 +0800

    Fixed optimistic response on archiving session documents

--- code/web/src/graphql/mutations/deleteSessionDocument.js
+++ code/web/src/graphql/mutations/deleteSessionDocument.js
@@ -1,12 +1,14 @@
 import { gql } from 'apollo-boost';
 
+import SESSION_DOCUMENT_BASIC_INFO from '../fragments/sessionDocumentBasicInfo';
+
 const DELETE_SESSION_DOCUMENT = gql`
   mutation DeleteSessionDocument($sd_id: UUID!) {
     deleteSessionDocument(sd_id: $sd_id) {
-      sd_id
-      file_name
+      ...SessionDocumentBasicInfo
     }
   }
+  ${SESSION_DOCUMENT_BASIC_INFO}
 `;
 
 export default DELETE_SESSION_DOCUMENT;
