643a958edd9b8a467215c088d85162417e53edbf
julianaubrey <2161495@slu.edu.ph>
2019-04-27 00:57:25 +0800

    Removed cache update on archives page after archiving a session

--- code/web/src/components/Client/SessionList/SessionListItem/SessionListItem.js
+++ code/web/src/components/Client/SessionList/SessionListItem/SessionListItem.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import DELETE_SESSION from '../../../../graphql/mutations/deleteSession';
 import { Mutation } from 'react-apollo';
 import CLIENT from '../../../../graphql/queries/client';
-import ARCHIVES from '../../../../graphql/queries/archives';
 
 import { cloneDeep } from 'apollo-utilities';
 
@@ -112,19 +111,6 @@ function SessionListItem(props) {
             client
           }
         });
-
-        const { archives } = cloneDeep(cache.readQuery({ query: ARCHIVES }));
-
-        archives.sessions.push(deleteSession);
-
-        cache.writeQuery({
-          query: ARCHIVES,
-          data: {
-            archives
-          }
-        });
-
-        props.enqueueSnackbar(session_name + ' archived!');
       }}
       optimisticResponse={{
         __typename: 'Mutation',
