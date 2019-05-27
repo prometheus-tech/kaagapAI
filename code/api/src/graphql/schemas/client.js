a1d4cbcb16956b03849ce52a5368f69e0e22df5b
erikatorio <2160120@slu.edu.ph>
2019-05-27 15:16:56 +0800

    Added Permanently Delete Archived Client functionality (Backend)

--- code/api/src/graphql/schemas/client.js
+++ code/api/src/graphql/schemas/client.js
@@ -57,6 +57,8 @@ export default `
 
     restoreClient(c_id: UUID!): Client!
 
+    permanentlyDeleteClient(c_id: UUID!): Client
+
     updateClientInformation(c_id: UUID!, fname: String!, lname: String!, birthdate: Date!, gender:[Gender!]!): Client!
   }
 `;
