449d7412aca2e54e7823979649226da9941b85d2
julianaubrey <2161495@slu.edu.ph>
2019-05-30 06:30:32 +0800

    Finished reset password

--- code/web/src/containers/ForgotPassword/FindEmail/FindEmail.js
+++ code/web/src/containers/ForgotPassword/FindEmail/FindEmail.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+
 import { withStyles } from '@material-ui/core/styles';
+
+import { Mutation } from 'react-apollo';
+import FORGOT_PASSWORD from '../../../graphql/mutations/forgotPassword';
+
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -8,6 +13,9 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import grey from '@material-ui/core/colors/grey';
 import forgot_password from '../../../assets/forgot_password.svg';
+import EmailIcon from '@material-ui/icons/Email';
+
+import { Link as RouterLink } from 'react-router-dom';
 
 const styles = theme => ({
   root: {
@@ -78,6 +86,10 @@ const styles = theme => ({
     boxShadow: 'none',
     marginTop: theme.spacing.unit * 5
   },
+  emailIcon: {
+    color: grey[500],
+    fontSize: 96
+  },
   title: {
     fontFamily: [
       '-apple-system',
@@ -134,56 +146,128 @@ const styles = theme => ({
 });
 
 class FindEmail extends Component {
+  state = {
+    currentStep: 0,
+    email: ''
+  };
+
+  inputChangeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  changeCurrentStepHandler = () => {
+    this.setState({
+      currentStep: 1
+    });
+  };
+
   render() {
     const { classes } = this.props;
+
+    const { email, currentStep } = this.state;
+
+    const ButtonSinginInLink = props => <RouterLink to="/signin" {...props} />;
+
     return (
-      <div className={classes.root}>
-        <Grid container>
-          <Grid item xs={7} className={classes.illustrationContainer}>
-            <img
-              src={forgot_password}
-              className={classes.illustration}
-              alt="forgot password"
-            />
-          </Grid>
-          <Grid item xs={5} className={classes.formContainer}>
-            <Grid container className={classes.forms}>
-              <Grid item xs={12} className={classes.messageContainer}>
-                <Typography variant="h6" className={classes.title}>
-                  Forgot Password?
-                </Typography>
-                <Typography variant="subtitle1" className={classes.subtitle}>
-                  Enter the email address associated with your account.
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl className={classes.forms}>
-                  <InputLabel shrink className={classes.bootstrapFormLabel}>
-                    Email address
-                  </InputLabel>
-                  <InputBase
-                    fullWidth
-                    classes={{
-                      root: classes.customInputRoot,
-                      input: classes.customkInput
-                    }}
+      <Mutation
+        mutation={FORGOT_PASSWORD}
+        errorPolicy="all"
+        onError={error => {
+          // Ignore error
+        }}
+        onCompleted={data => {
+          this.changeCurrentStepHandler();
+        }}
+      >
+        {(forgotPassword, { loading, error }) => {
+          return (
+            <div className={classes.root}>
+              <Grid container>
+                <Grid item xs={7} className={classes.illustrationContainer}>
+                  <img
+                    src={forgot_password}
+                    className={classes.illustration}
+                    alt="forgot password"
                   />
-                </FormControl>
+                </Grid>
+                <Grid item xs={5} className={classes.formContainer}>
+                  {currentStep === 0 ? (
+                    <Grid container className={classes.forms}>
+                      <Grid item xs={12} className={classes.messageContainer}>
+                        <Typography variant="h6" className={classes.title}>
+                          Forgot Password?
+                        </Typography>
+                        <Typography
+                          variant="subtitle1"
+                          className={classes.subtitle}
+                        >
+                          Enter the email address associated with your account.
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl className={classes.forms}>
+                          <InputLabel
+                            shrink
+                            className={classes.bootstrapFormLabel}
+                          >
+                            Email address
+                          </InputLabel>
+                          <InputBase
+                            fullWidth
+                            classes={{
+                              root: classes.customInputRoot,
+                              input: classes.customkInput
+                            }}
+                            value={email}
+                            name="email"
+                            onChange={this.inputChangeHandler}
+                          />
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <ButtonBase
+                          type="button"
+                          className={classes.resetPassword}
+                          onClick={() => {
+                            forgotPassword({ variables: { email: email } });
+                          }}
+                        >
+                          Reset password
+                        </ButtonBase>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <ButtonBase
+                          component={ButtonSinginInLink}
+                          type="button"
+                          className={classes.return}
+                        >
+                          Back to sign in
+                        </ButtonBase>
+                      </Grid>
+                    </Grid>
+                  ) : (
+                    <Grid container>
+                      <Grid item xs={12}>
+                        <EmailIcon className={classes.emailIcon} />
+                        <Typography variant="h6" className={classes.title}>
+                          Check your email
+                        </Typography>
+                        <Typography
+                          variant="subtitle1"
+                          className={classes.subtitle}
+                        >
+                          A reset password link was sent to your email. Use the
+                          link to reset your password.
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  )}
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <ButtonBase type="button" className={classes.resetPassword}>
-                  Reset password
-                </ButtonBase>
-              </Grid>
-              <Grid item xs={12}>
-                <ButtonBase type="button" className={classes.return}>
-                  Back to sign in
-                </ButtonBase>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
+            </div>
+          );
+        }}
+      </Mutation>
     );
   }
 }
