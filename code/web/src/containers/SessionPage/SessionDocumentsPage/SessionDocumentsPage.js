2ad19995ac604667d32c28875df1cbe1b8c1c9bc
julianaubrey <2161495@slu.edu.ph>
2019-05-29 14:02:00 +0800

    Fixed missing field in rename session mutation on frontend

--- code/web/src/containers/SessionPage/SessionDocumentsPage/SessionDocumentsPage.js
+++ code/web/src/containers/SessionPage/SessionDocumentsPage/SessionDocumentsPage.js
@@ -182,6 +182,7 @@ class SessionDocumentsPage extends Component {
               closed={sessionDocumentRenameDialogClosed}
               sessionDocument={selectedSessionDocument}
               selectedSessionDocumentUpdated={selectedSessionDocumentUpdated}
+              sessionId={session_id}
             />
           </Auxilliary>
         ) : null}
