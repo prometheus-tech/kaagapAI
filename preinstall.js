1dbbc5f8fd8df6ae5bde3e7b26c61fda8fd9182f
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-12-04 14:38:12 -0700

    Fixed path of google-crendentials

--- preinstall.js
+++ preinstall.js
@@ -1,10 +1,8 @@
 require('dotenv').config({ path: './.env' });
 const fs = require('fs');
 
-console.log('Generating Google credentials');
-
 fs.writeFile(
-  'config/google-credentials.json',
+  '/config/google-credentials.json',
   process.env.GOOGLE_CONFIG,
   err => {}
 );
