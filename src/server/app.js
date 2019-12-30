608dedaded62dc1846d7961680936c11ef64fef3
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-12-30 15:14:54 -0700

    Fixed path for serving index.html in prod

--- src/server/app.js
+++ src/server/app.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(cors());
 
 if (environment !== 'development') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
